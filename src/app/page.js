e1dbc3548859b4d110e0c33df9539bfd36dc01ab
sachu29 <sachusaran29.94@gmail.com>
2025-03-16 20:43:17 +0530

    F

--- src/app/page.js
+++ src/app/page.js
@@ -3,23 +3,23 @@ import "./globals.css"
 import Navbar from "../components/Navbar";
 import Main from '../components/Main';
 import { useState, useEffect } from "react";
-import { WishlistProvider } from './../context/WishListContext'; 
+import { WishlistProvider } from './../context/WishListContext';
 import Footer from './../components/Footer';
 import { fetchCategory, fetchProducts } from "./common";
 import { Pagination } from 'antd';
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState('');
-  const [allProducts, setAllProducts] = useState([]); 
-  const [filteredProducts, setFilteredProducts] = useState([]); 
+  const [allProducts, setAllProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [productsPerPage, setProductsPerPage] = useState(24);
   const [totalProducts, setTotalProducts] = useState(0);
 
-  async function fetchAllProducts (query) {
-    try{
-      const data = await fetchProducts({query:query}); 
+  async function fetchAllProducts(query) {
+    try {
+      const data = await fetchProducts({ query: query });
 
       setAllProducts(data.products);
       setFilteredProducts(data.products);
@@ -30,30 +30,31 @@ export default function Home() {
         behavior: "smooth",
       });
 
-    }catch(err){
+    } catch (err) {
       console.error(err)
     }
   }
 
-  useEffect( () => {;
-    if(allProducts.length === 0){
+  useEffect(() => {
+    ;
+    if (allProducts.length === 0) {
       fetchAllProducts('?limit=24&skip=0');
     }
-  },[]);
+  }, []);
 
-  const handleSearch = async(search) => {
-    
+  const handleSearch = async (search) => {
+
     setSearchQuery(search);
     setSelectedCategory(null);
 
-    let query = `${search ? '/search?q='+search +'&' : '?' }limit=${productsPerPage}&skip=0`;
+    let query = `${search ? '/search?q=' + search + '&' : '?'}limit=${productsPerPage}&skip=0`;
 
     await fetchAllProducts(query);
 
   };
 
   const handleCategoryChange = async (category) => {
-    
+
     setSelectedCategory(category);
 
     const data = await fetchCategory(category);
@@ -67,21 +68,21 @@ export default function Home() {
     setCurrentPage(page);
     let skip = (page - 1) * productsPerPage;
 
-    let query = `${selectedCategory ? '/category' : ''}${searchQuery ? '/search?q='+searchQuery : '?'}&limit=${productsPerPage}&skip=${skip}`;
+    let query = `${selectedCategory ? '/category' : ''}${searchQuery ? '/search?q=' + searchQuery : '?'}&limit=${productsPerPage}&skip=${skip}`;
 
     fetchAllProducts(query);
   };
 
   return (
     <>
-    <WishlistProvider> 
-       <Navbar onSearch={handleSearch} />
-        <Main onCategoryChange={handleCategoryChange} cardData={filteredProducts} category={selectedCategory} /> 
+      <WishlistProvider>
+        <Navbar onSearch={handleSearch} />
+        <Main onCategoryChange={handleCategoryChange} cardData={filteredProducts} category={selectedCategory} />
         <div className="flex justify-center w-full my-4">
-          <Pagination defaultCurrent={currentPage} total={totalProducts} onChange={onPageChange} defaultPageSize={productsPerPage}/>
+          <Pagination defaultCurrent={currentPage} total={totalProducts} onChange={onPageChange} defaultPageSize={productsPerPage} />
         </div>
         <Footer />
-    </WishlistProvider>
+      </WishlistProvider>
     </>
   );
 }
